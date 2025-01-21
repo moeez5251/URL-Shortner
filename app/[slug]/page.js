11f56488237c3d15b1a2691e0d484ade464c573a
moeez5251 <moeez66656@gmail.com>
2025-01-21 20:28:40 +0500

    Fix app crashing and database issues 🛠️✅

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -1,15 +1,33 @@
 import { redirect } from 'next/navigation'
-import { Client, Databases } from 'appwrite';
+import { Client, Databases, Query } from 'appwrite';
 const client = new Client();
 client.setProject(process.env.NEXT_PUBLIC_PROJECT)
 const databases = new Databases(client);
+let limit = 100;  
+let offset = 0;
+let allDocuments = [];
+let hasMore = true;
 export default async function Page({ params }) {
     const slug = await params;
-    let response= await databases.listDocuments(
-        process.env.NEXT_PUBLIC_DB_ID,
-        process.env.NEXT_PUBLIC_COLLECTION,
-    )
-    let red =  response.documents.find(e => {
+   while (hasMore) {
+        let response = await databases.listDocuments(
+            process.env.NEXT_PUBLIC_DB_ID,
+            process.env.NEXT_PUBLIC_COLLECTION,
+            [
+                Query.limit(limit),
+                Query.offset(offset)
+            ]
+        );
+
+    allDocuments.push(...response.documents);
+
+        if (response.documents.length < limit) {
+            hasMore = false;
+        } else {
+            offset += limit; 
+        }
+    }
+    let red = allDocuments.find(e => {
         return e.keyword === slug.slug;
     })
     if(red){
